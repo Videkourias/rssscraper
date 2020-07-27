3bbc007e084af6141887f1c7fd1f72ad6379faa7
Dean <videkourias@gmail.com>
2020-07-27 16:33:20 -0400

    Added CLI to input search terms

--- scraper.py
+++ scraper.py
@@ -39,26 +39,60 @@ def rss(feed, src):
                         'src': src
                     }
                     article_list.append(article)
-                    #print('Title: {}\nSource: {}\n'.format(title, src))
-                    #print('=================================\n')
+                    # print('Title: {}\nSource: {}\n'.format(title, src))
+                    # print('=================================\n')
 
         return save_function(article_list)
 
     except Exception as e:
-        print('Windsor Star scraping job failed. See exception:')
+        print('{} scraping job failed. See exception:'.format(src))
         print(e)
 
 
-print('Starting scraping\n')
+def main(argv):
+    # Keyword lists
+    kwand = []
+    kwor = []
 
-# Load RSS feed data from JSON file
-with open('feeds.json', 'r') as f:
-    data = f.read()
-feeds = json.loads(data)
+    # Handle arguments
+    try:
+        opts, args = getopt.getopt(argv, "h:a:o:", ['help', 'and=', 'or='])
+    except getopt.GetoptError:
+        print('scraper.py -a "kw kw k_w..." -o "kw kw k_w..."')
+        sys.exit(2)
 
-# Scrape all feeds
-for f in feeds['feeds']:
-    print(f['link'], '\n', f['src'])
-    rss(f['link'], f['src'])
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            print('scraper.py -a "kw kw k_w..." -o "kw kw k_w..."')
+            sys.exit()
+        elif opt in ("-a", "--and"):
+            words = arg.split(' ')
+            for w in words:
+                kwand.append(w.replace('_', ' '))
+        elif opt in ("-o", "--or"):
+            words = arg.split(' ')
+            for w in words:
+                kwor.append(w.replace('_', ' '))
 
-print('Finished scraping')
+    if kwand or kwor:
+        print('Starting scraping\n')
+        print('Searching for articles with title containing all of:\n{}\nor any of:\n{}\n'.format(kwand, kwor))
+
+        # Load RSS feed data from JSON file
+        with open('feeds.json', 'r') as f:
+            data = f.read()
+        feeds = json.loads(data)
+
+        # Scrape all feeds
+        for f in feeds['feeds']:
+            pass
+            # rss(f['link'], f['src'])
+
+        print('Finished scraping')
+
+    else:
+        print("No search terms(keywords) have been provided, try using -a or -o. Exiting...")
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
